615b476b49b484d01562f6251fec2172de925d8a
Brayan Silva <brayan_silva82151@elpoli.edu.co>
2022-01-08 14:40:24 -0500

    Directriz 12 texto enriquecido

--- src/components/private/FormAnswer.jsx
+++ src/components/private/FormAnswer.jsx
@@ -1,27 +1,34 @@
-import useFormData from '../../hooks/UseFormData'
 import { postAnswer } from '../../app/middleware/payloadQuestions';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useState } from 'react';
+import TextEditor from '../../hooks/TextEditor';
 
 const FormAnswer = ({idQuestion}) => {
 
     const state =useSelector(state=>state.auth)
+    const dispatch = useDispatch();
 
-    const{form, formData, updateFormData} = useFormData();
+    const [body, setBody] = useState("");
 
     const submitForm = (e) => {
         e.preventDefault();
-        postAnswer(formData)
+        const formData = { 
+            userId: state.user.uid,
+            questionId: idQuestion,
+            answer: body,
+            position: 1
+        }
+        dispatch(postAnswer(formData));
+        setBody("");
       }
 
     return(
 
         <div>
-            <form ref={form} onSubmit={submitForm} onChange={updateFormData}>
+            <form onSubmit={submitForm}>
                 <h1>Respuestas</h1>
                 <label>Añade una respuesta.</label>
-                <input required name="answer" type="text" placeholder='Ingresa acá una respuesta'></input>
-                <input hidden name="userId" type="text" value={state.user.uid} ></input>
-                <input hidden name="questionId" type="text" value={idQuestion} ></input>
+                <TextEditor body={body} setBody={setBody}/>
                 <button type="submit">Enviar Respuesta</button>
             </form>
         </div>
@@ -30,4 +37,4 @@ const FormAnswer = ({idQuestion}) => {
 
 }
 
-export default FormAnswer
+export default FormAnswer;
---
+++ src/hooks/TextEditor.js
@@ -0,0 +1,58 @@
+import ReactQuill from 'react-quill';
+import '../../node_modules/react-quill/dist/quill.snow.css';
+import { useDispatch } from 'react-redux';
+
+
+const TextEditor = ({body, setBody}) => {
+
+    const dispatch = useDispatch();
+
+    const handlerBody = e => {
+        setBody(e);
+    }
+
+    return (
+        <div>
+            <form>
+                <ReactQuill 
+                    placeholder='Ingresa una pregunta/respuesta'
+                    modules={TextEditor.modules}
+                    formats={TextEditor.formats}
+                    onChange={handlerBody}
+                    value={body}
+                />
+            </form>
+        </div>
+    )
+}
+
+TextEditor.modules = {
+    toolbar: [
+        [{ header: "1" }, {header: "2"}, {header: [3, 4, 5, 6]}, {font: []}],
+        [{ size: [] }],
+        [ "bold", "italic", "underline", "strike", "blockquote" ],
+        [{ list: "ordered", }, { list: "bullet" }],
+        ["link", "image", "video"],
+        ["clean"],
+        ["code-block"],
+    ],
+};
+
+TextEditor.formats = [
+	"header",
+	"font",
+	"size",
+	"bold",
+	"italic",
+	"underline",
+	"strike",
+	"blockquote",
+	"list",
+	"bullet",
+	"link",
+	"image",
+	"video",
+	"code-block"
+];
+
+export default TextEditor;
